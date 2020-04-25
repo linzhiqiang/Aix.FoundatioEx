e57398569e6741c9123a4f3efd3506914574a0fc
linzhiqiang <360779475@qq.com>
2020-04-25 20:40:22 +0800

    升级

---
+++ src/Aix.FoundatioEx.Kafka/Utils/ObjectUtils.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace Aix.FoundatioEx.Kafka.Utils
+{
+    public class ObjectUtils
+    {
+        /// <summary>
+        /// 把obj的属性 填充到target上
+        /// </summary>
+        /// <param name="target"></param>
+        /// <param name="obj"></param>
+        public static void Assign(object target, object obj)
+        {
+            if (target == null || obj == null) return;
+
+            var pros= obj.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance | BindingFlags.GetProperty);
+            var targetType = target.GetType();
+            foreach (var item in pros)
+            {
+             var temp=   targetType.GetProperty(item.Name, BindingFlags.Public | BindingFlags.Instance | BindingFlags.SetProperty | BindingFlags.IgnoreCase);
+                if (temp != null)
+                {
+                    temp.SetValue(target, item.GetValue(obj));
+                }
+            } 
+        }
+    }
+}
